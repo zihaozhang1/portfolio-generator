e29eaed6f24eeaca7fce2a389cddf922ad527f72
Zihao <zihaozhang1@gmail.com>
2021-01-31 03:55:18 -0500

    questions

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const inquirer = require('inquirer');
-console.log(inquirer)
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
 
 // const fs = require('fs');
