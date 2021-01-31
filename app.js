8d7362d4a6976d32c1ff399dab995747d6db706c
Zihao <zihaozhang1@gmail.com>
2021-01-31 02:25:52 -0500

    added inquire

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+console.log(inquirer)
 
 
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+// const pageHTML = generatePage(name, github);
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
